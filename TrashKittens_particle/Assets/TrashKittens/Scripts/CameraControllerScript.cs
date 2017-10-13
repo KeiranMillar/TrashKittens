1ca34cb31a8674df778c0c53117df8c837cb8048
Rory Kerr <1401360@uad.ac.uk>
2017-10-13 14:50:16 +0100

    Rory - UI Test Scene created.

---
+++ TrashKittens_particle/Assets/TrashKittens/Scripts/CameraControllerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// script intended to control the camera in a world space ui test
+// currently placeholder, will likely be renamed / replaced
+
+public class CameraControllerScript : MonoBehaviour {
+
+	private Camera thisCamera;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
