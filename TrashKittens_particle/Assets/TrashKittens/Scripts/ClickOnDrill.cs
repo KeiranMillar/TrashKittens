3ff10d458858836d3791f025711684cd646a22f0
Keiran Millar <keiranmillar@gmail.com>
2017-10-26 15:08:50 +0100

    Added broken purchasing of upgrades

---
+++ TrashKittens_particle/Assets/TrashKittens/Scripts/ClickOnDrill.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOnDrill : MonoBehaviour {
+
+	public int price = 5;
+	GameObject drill = GameObject.Find("Quad");
+	//var resourcesScript = drill.GetComponent<ResourceCollection>();
+	float resources = drill.GetComponent<ResourceCollection>().resources;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	// When the drill is clicked
+	void OnMouseDown()
+	{
+		// Do something
+		Debug.Log ("Click");
+		if (drill.GetComponent< ResourceCollection > ().resources >= price) 
+		{
+			drill.GetComponent< ResourceCollection > ().resources -= price;
+			drill.GetComponent< ResourceCollection > ().resourceModifier += 2;
+			price *= 2;
+		} 
+		else 
+		{
+			Debug.Log ("Need more Money");
+		}
+	}
+
+}
--- TrashKittens_particle/Assets/TrashKittens/Scripts/ResourceCollection.cs
+++ TrashKittens_particle/Assets/TrashKittens/Scripts/ResourceCollection.cs
@@ -6,11 +6,12 @@ public class ResourceCollection : MonoBehaviour
 {
 	public float resources = 0;
 	public float resourcesPerSec = 1;
-	public float lastUpdate = 0;
+	public float resourceModifier = 1;
+	float lastUpdate = 0;
 	// Use this for initialization
 	void Start () 
 	{
-		
+		Debug.Log("Resources = " + resources.ToString());
 	}
 	
 	// Update is called once per frame
@@ -18,7 +19,7 @@ public class ResourceCollection : MonoBehaviour
 	{
 		if(Time.time - lastUpdate >= 1f)
 		{
-			resources += resourcesPerSec;
+			resources += resourcesPerSec * resourceModifier;
 			lastUpdate = Time.time;
 			Debug.Log("Resources = " + resources.ToString());
 		}
