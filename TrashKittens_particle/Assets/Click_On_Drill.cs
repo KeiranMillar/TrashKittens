e6478dac18f57d933f677afd4a473d82aaeff8d3
Keiran Millar <keiranmillar@gmail.com>
2017-10-26 13:27:34 +0100

    Added functionality to click on drill

---
+++ TrashKittens_particle/Assets/Click_On_Drill.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click_On_Drill : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnMouseDown()
+	{
+		Debug.Log ("Click");
+	}
+
+}
