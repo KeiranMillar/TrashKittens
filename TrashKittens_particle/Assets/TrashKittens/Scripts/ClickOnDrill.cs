df506e4f4c03f3bde00fc0a938870b77d2243b2f
Keiran Millar <keiranmillar@gmail.com>
2017-10-27 15:12:14 +0100

    Added - Reloading the scene when the health gets to 0. Also upgrading the drill resets the health

--- TrashKittens_particle/Assets/TrashKittens/Scripts/ClickOnDrill.cs
+++ TrashKittens_particle/Assets/TrashKittens/Scripts/ClickOnDrill.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class ClickOnDrill : MonoBehaviour {
 
 	public int price = 5;
-
-
+	public GameObject drill;
 
 	// Use this for initialization
 	void Start () 
@@ -17,22 +16,23 @@ public class ClickOnDrill : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		
+
 	}
 
 	// When the drill is clicked
 	void OnMouseDown()
 	{
-		GameObject drill = GameObject.Find("Quad");
 		ResourceCollection resourcesScript = drill.GetComponent<ResourceCollection>();
-		float resources = drill.GetComponent<ResourceCollection>().resources;
+		HealthbarPlaceholderScript healthScript = drill.GetComponent<HealthbarPlaceholderScript> ();
+		float resources = resourcesScript.resources;
 		// Do something
-		Debug.Log ("Click");
-		if (drill.GetComponent< ResourceCollection > ().resources >= price) 
+		//Debug.Log ("Click");
+		if (resources >= price) 
 		{
-			drill.GetComponent< ResourceCollection > ().resources -= price;
-			drill.GetComponent< ResourceCollection > ().resourceModifier *= 2;
+			resources -= price;
+			resourcesScript.resourceModifier *= 2;
 			price *= 2;
+			healthScript.health = 100;
 		} 
 		else 
 		{
