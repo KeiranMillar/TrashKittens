342ba5a6cc56f62931346325d0ecaaeaf1402626
Matthew Wallace <1502616@abertay.ac.uk>
2017-11-02 12:51:30 +0000

    FRESH START

---
+++ Prototype/Assets/TrashKittens/Scripts/ResourceCollection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceCollection : MonoBehaviour 
+{
+	public float resources = 0;
+	public float resourcesPerSec = 1;
+	public float resourceModifier = 1;
+	public enum resourceCollectionType{Periodical,Constant};
+	public resourceCollectionType collectionType;
+	float lastUpdate = 0;
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log("Resources = " + resources.ToString());
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (collectionType == resourceCollectionType.Constant) 
+		{
+			resources += resourcesPerSec * resourceModifier * Time.deltaTime;
+			Debug.Log ("Resources = " + resources.ToString ());
+		} 
+		else if (collectionType == resourceCollectionType.Periodical)
+		{
+			if (Time.time - lastUpdate >= 1f) 
+			{
+				resources += resourcesPerSec * resourceModifier;
+				lastUpdate = Time.time;
+				Debug.Log ("Resources = " + resources.ToString ());
+			}
+		}
+	}
+}
