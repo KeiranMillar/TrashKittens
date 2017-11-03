58ae5b599e06e0e579987c2d8e78de5852b3736f
Keiran Millar <keiranmillar@gmail.com>
2017-11-03 14:37:51 +0000

    Added - Pooling Script

---
+++ _Keiran/Assets/ClickOnDrill.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOnDrill : MonoBehaviour {
+
+	public int price = 5;
+	public GameObject drill;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	// When the drill is clicked
+	void OnMouseDown()
+	{
+		ResourceCollection resourcesScript = drill.GetComponent<ResourceCollection>();
+		HealthbarPlaceholderScript healthScript = drill.GetComponent<HealthbarPlaceholderScript> ();
+		float resources = resourcesScript.resources;
+		// Do something
+		//Debug.Log ("Click");
+		if (resources >= price) 
+		{
+			resources -= price;
+			resourcesScript.resourceModifier *= 2;
+			price *= 2;
+			healthScript.health = 100;
+		} 
+		else 
+		{
+			Debug.Log ("Need more Money");
+		}
+	}
+
+}
---
+++ _Keiran/Assets/GameStateManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// A script intended as a game state manager
+// Initially just tracks if game is paused or not
+
+public class GameStateManager : MonoBehaviour {
+
+	// Game state enum
+	public enum GameState {active, paused};
+
+	// public variables
+	public GameState currentGameState;
+
+	// Enters paused State (currently does nothing but change time scale, changes in resource and health scripts needed)
+	public void PauseGame () 
+	{
+		currentGameState = GameState.paused;
+		Time.timeScale = 0.0f;
+	}
+
+	// Enters active state
+	public void UnpauseGame()
+	{
+		currentGameState = GameState.active;
+		Time.timeScale = 1.0f;
+	}
+
+}
