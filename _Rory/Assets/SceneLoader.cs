9c7a252af1e0b328baf1950d5da0e99682ed8682
MoogTheThird <1401360@abertay.ac.uk>
2017-11-16 01:00:33 +0000

    Ability to load prototype scene from "Rory" main menu test scene added.

---
+++ _Rory/Assets/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// script that should allow for scenes to be easily loaded
+
+public class SceneLoader : MonoBehaviour {
+
+	// Public Variables, check build settings for appropriate values
+
+	public int mainMenuScene;
+	public int prototypeScene;
+	
+	public void loadMainMenu ()
+	{
+		SceneManager.LoadScene (mainMenuScene, LoadSceneMode.Single);
+	}
+
+	public void loadPrototype ()
+	{
+		SceneManager.LoadScene (prototypeScene, LoadSceneMode.Single);
+	}
+}
