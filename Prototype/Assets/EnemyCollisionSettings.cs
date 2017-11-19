2b5a03ddc1747678c32c0a1edfeeefc6e0b23c8e
MoogTheThird <1401360@abertay.ac.uk>
2017-11-19 20:37:54 +0000

    Enemies will no longer collide with one another by default. Can be changed by setting "allow enemy on enemy collisions" to true on the spawn manager.

---
+++ Prototype/Assets/EnemyCollisionSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollisionSettings : MonoBehaviour {
+
+	// disables collisions between enemies when set to false.
+	// make sure the aliens are set to the "enemy" layer in the editor
+
+	public bool allowEnemyOnEnemyCollision = false;
+
+	void Start () 
+	{
+		if (allowEnemyOnEnemyCollision == false) 
+		{
+			Physics.IgnoreLayerCollision (9, 9, true);
+		}
+	}
+}
