c9ffc333b0e898e86e6872446ad20d654636e95c
Miro <32368860+pixel85@users.noreply.github.com>
2017-12-01 13:37:23 +0000

    Ambience sound added. Music is delayed by 2 seconds and then smoothly fade in by next 4 seconds

---
+++ Prototype/Assets/TrashKittens/Audio/Music/Level01/AudioFadeIn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioFadeIn : MonoBehaviour {
+
+	/*
+		Audio Fade In
+ 
+	*/
+
+	AudioSource myAudio;
+
+	void Start () {
+
+		myAudio = GetComponent<AudioSource> ();
+
+	}
+	public int approxSecondsToFade = 5;
+
+	void FixedUpdate ()
+	{
+		if (myAudio.volume < 1)
+		{
+			myAudio.volume = myAudio.volume + (Time.deltaTime / (approxSecondsToFade + 5));
+		}
+		else
+		{
+			Destroy (this);
+		}
+	}
+}
