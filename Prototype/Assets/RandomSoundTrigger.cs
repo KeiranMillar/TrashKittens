b63c58bcef48e0ac9dded0491ccf44a008021a55
Miro <battleangelofficial@gmail.com>
2018-02-08 12:29:26 +0000

    Splat audio and random noise script added. Currently WIP.

---
+++ Prototype/Assets/RandomSoundTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSoundTrigger : MonoBehaviour {
+
+	public AudioSource source;
+	public AudioClip[] clips;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter()
+	{
+		source.clip = clips[Random.Range(0, clips.Length)];
+		source.Play();
+	}
+}
