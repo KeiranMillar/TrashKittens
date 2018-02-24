97d7282f5b47c306bba30bc95d8b106f84bedc5b
MoogTheThird <1401360@abertay.ac.uk>
2018-02-24 17:51:40 +0000

    Added new model with animation. Added small script used to set the upgrade level of the drill for animation purposes.

---
+++ Prototype/Assets/DrillAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrillAnimation : MonoBehaviour {
+
+	public Animator anim;
+
+	// sets the upgrade animations to use
+	public void setUpgradeLevel(int upgradeLevel)
+	{
+		anim.SetInteger("UpgradeLevel", upgradeLevel);
+	}
+
+	public int getUpgradeLevel()
+	{
+		return anim.GetInteger("UpgradeLevel");
+	}
+
+
+}
