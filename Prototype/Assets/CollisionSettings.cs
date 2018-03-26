f14b60c8e2039ffd976ba7275521d02ef5229958
MoogTheThird <1401360@abertay.ac.uk>
2018-03-26 19:25:55 +0100

    Bullets no longer collide withr eousrce pickups.

--- Prototype/Assets/CollisionSettings.cs
+++ Prototype/Assets/CollisionSettings.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyCollisionSettings : MonoBehaviour {
+public class CollisionSettings : MonoBehaviour {
 
 	// disables collisions between enemies when set to false.
 	// make sure the aliens are set to the "enemy" layer in the editor
@@ -13,12 +13,10 @@ public class EnemyCollisionSettings : MonoBehaviour {
 	{
 		if (allowEnemyOnEnemyCollision == false) 
 		{
-			Physics.IgnoreLayerCollision (9, 9, true);
 			Physics2D.IgnoreLayerCollision (9, 9, true);
-			Physics.IgnoreLayerCollision (11, 9, true);
 			Physics2D.IgnoreLayerCollision (11, 9, true);
-			Physics.IgnoreLayerCollision (11, 10, true);
 			Physics2D.IgnoreLayerCollision (11, 10, true);
+			Physics2D.IgnoreLayerCollision (11, 12, true);
 		}
 	}
 }
